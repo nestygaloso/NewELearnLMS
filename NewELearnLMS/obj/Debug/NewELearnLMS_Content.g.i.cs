59440726012d9872b902b9f88412199c522e7a75
Nesty Galoso <nesty.galoso89@gmail.com>
2024-08-01 02:21:30 -0400

    Update Project
    Update Project

--- NewELearnLMS/obj/Debug/NewELearnLMS_Content.g.i.cs
+++ NewELearnLMS/obj/Debug/NewELearnLMS_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("customercare2.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("csharp.jpg")]
 
 
